b5e7d92d01109a468b9938627129f9a58e6d9b47
shiweii <shiwei_ou@hotmail.com>
2014-12-24 16:39:36 +0800

    Update Config Password

--- app/api/config.php
+++ app/api/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password1');
+define('DB_PASSWORD', 'password');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ohana');
 
