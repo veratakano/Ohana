03cdbf947ec552bc5cb580cf7d04aa622477f56f
shiweii <shiwei_ou@hotmail.com>
2015-02-06 14:45:13 +0800

    code clean up

--- app/js/controllers/dashboardCtrl.js
+++ app/js/controllers/dashboardCtrl.js
@@ -2,62 +2,27 @@
 'use strict';
 
 app.controller('dashboardCtrl', ['$rootScope','$scope','$route','ngDialog','loginService','treeService', function($rootScope,$scope,$route,ngDialog,loginService,treeService){
-
-	$scope.txt='Dashboard';
 	
 	$scope.logout=function(){
 		loginService.logout();
 	};
 
+  // Open Popup for members
   $scope.openDialog = function (id,title,gender) {
-
         ngDialog.open({
           template: 'userDialogId',
           className: 'ngdialog-theme-default ngdialog-theme-custom',
           data: {id: id, title: title, gender: gender},
-          controller: 'SomeController'
+          controller: 'dialogCtrl'
         });
   };
-
-  $rootScope.$on('ngDialog.closed', function (e, $dialog) {
-    
-    var gen3 = ({
-              "gen": "3",
-              'nodes': [
-                  {
-                    'id': 5,
-                    'title': 'Grandson',
-                    'text': '',
-                    'top': 200,
-                    'left': 20,
-                  },
-                  {
-                    'id': 6,
-                    'title': 'Granddaughter',
-                    'text': '',
-                    'top': 20,
-                    'left': 500,
-                  },
-                ]
-         });
-
-    var connG31 = ({'from': 'node5', 'to': 'node2', 'pos1':'TopCenter', 'pos2':'Bottom'});
-    var connG32 = ({'from': 'node6', 'to': 'node2', 'pos1':'TopCenter', 'pos2':'Bottom'});
-
-    //$scope.flowchart.generations.push(gen3);
-    //$scope.flowchart.connections.push(connG31);
-    //$scope.flowchart.connections.push(connG32);
-
-    //$route.reload();
-  });
-
-  //$scope.flowchart = treeService.getTreeData();
 
   $scope.$on('onRepeatLast', function(scope, element, attrs){  
        var connections = $rootScope.flowchart.connections;
        angular.forEach(connections, function(idx, elm) {
             //jsPlumb set container
            jsPlumb.setContainer($("#treeView"));
+           //jsPlumb Draw relation
            jsPlumb.connect({
                 source: idx.from,
                 target: idx.to,
@@ -66,34 +31,12 @@ app.controller('dashboardCtrl', ['$rootScope','$scope','$route','ngDialog','logi
                 connector:[ "Flowchart" ]
             });
         });
-
-
-       /*jsPlumb.connect({
-                source: idx.from,
-                target: idx.to,
-                detachable: false,
-                paintStyle: {
-                  strokeStyle: '#005b0f',
-                  lineWidth: '1'
-                },
-                hoverPaintStyle: {
-                  strokeStyle: '#ff0000',
-                  lineWidth: '2'
-                },
-                dragOptions: {
-                  cursor: "pointer",
-                  zIndex: 2000
-                },
-                endpoint: "Blank",
-                endpoint:[ "Rectangle", { width:10, height:10 }, "Rectangle", { width:10, height:8 } ],
-                anchors:[idx.pos1, idx.pos2],
-                connector:[ "Flowchart", { alwaysRespectStubs: true, cornerRadius: 0, gap:0,stub:[30, 30]} ]
-            });*/
     });
    
 }])
 
-app.controller('SomeController', ['$scope', '$location', 'ngDialog', function($scope,$location,ngDialog){
+// Open Popup Controller
+app.controller('dialogCtrl', ['$scope', '$location', 'ngDialog', function($scope,$location,ngDialog){
             $scope.openEdit = function(){
                ngDialog.close();
                ngDialog.open({
