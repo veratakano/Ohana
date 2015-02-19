a09495c6487a78b22105fb5b1b408320e6c74b77
Vera Aung <vera.aung@gmail.com>
2015-02-19 23:13:34 +0800

    Tree
    Latest Tree

--- app/partials/Tree.php
+++ app/partials/Tree.php
@@ -42,7 +42,6 @@
             width: 4em;  			
 			margin: 0 auto; 
         }  
-  
     </style>  
   
 	</head>  	  
@@ -126,29 +125,25 @@
 				if($member_values[3] != 0) {
 					if($member_values[2] == 'M') 
 						// change colour of males
-						echo '<div class="windowMale" style="top: '. $y .'%; left: '. $x .'%" id="container0" ng-click="openDialog('. $memberID . ')">  
+						echo '<div class="windowMale" style="top: '. ($y*7) .'px; left: '. ($x*7) .'px" id="container0" ng-click="openDialog('. $memberID . ')">  
 							<div>'. $memberID . '</div>
 						</div> ';
 					else 
 						// change colours of females
-						echo '<div class="windowFemale" style="top: '. $y .'%; left: '. $x .'%" id="container0" ng-click="openDialog('. $memberID . ')">  
+						echo '<div class="windowFemale" style="top: '. ($y*7) .'px; left: '. ($x*7) .'px" id="container0" ng-click="openDialog('. $memberID . ')">  
 							<div>'. $memberID . '</div>
 						</div> ';					
 				}
-				else
+				else {
 					// change colour of passed-away
-					echo '<div class="window" style="top: '. $y .'%; left: '. $x .'%" id="container0" ng-click="openDialog('. $memberID . ')">  
+					echo '<div class="window" style="top: '. ($y*7) .'px; left: '. ($x*7) .'px" id="container0" ng-click="openDialog('. $memberID . ')">  
 						<div>'. $memberID . '</div>
 					</div> ';
+				}
 					//$member_values[0]
 				/** colour **/
 
 				/** lines **/	
-				
-				
-				
-				
-				
 				
 				if ($fatherID != 0 and $motherID != 0) {
 					
@@ -161,9 +156,9 @@
 						$count = $count + 1;
 						
 					}
-					else {
-						$x1 = $x;
-					}
+					
+					$x1 = $x;
+					
 					
 					$display_array[$count] = array($x0, $x1, $y);
 				}
@@ -173,53 +168,65 @@
 				if (array_key_exists($memberID, $relation_array)) {
 					if ($fatherID != 0 and $motherID != 0)
 						// kid's vertical line
-						echo '<div style="top:'. ($y-3) .'%; left:'. ($x+2) .'%; position: absolute;">  
-							<div style="width: 1px; height: 20px; background-color: grey;"></div>
+						echo '<div style="top:'. (($y-3) *7) .'px; left:'. (($x+2) *7) .'px; position: absolute;">  
+							<div style="width: 2px; height: 20px; background-color: grey;"></div>
 						</div> ';
 				}
 				
 				if ($spouseID <> 0) {
 					
 					// spouse downword vertical line
-					echo '<div style="top:'. ($y+9) .'%; left:'. ($x+2) .'%; position: absolute;">  
-						<div style="width: 1px; height: 20px; background-color: grey;"></div>
+					echo '<div style="top:'. (($y+9) *7) .'px; left:'. (($x+2) *7) .'px; position: absolute;">  
+						<div style="width: 2px; height: 20px; background-color: grey;"></div>
 					</div> ';
 						
-					$coor_values = $coor_array[$spouseID];
-					$spouse_x = $coor_values[3];
-						
-					if($x < $spouse_x) {
-						// spouse horizontal line
-						echo '<div style="top:'. ($y+12) .'%; left:'. ($x+2) .'%; position: absolute;">  
-							<div style="width: 256px; height: 1px; background-color: grey;"></div>
-						</div> ';
+					if(array_key_exists($memberID, $relation_array)) {
+						$coor_values = $coor_array[$spouseID];
+						$spouse_x = $coor_values[3];
 							
-					}	
+						if($x < $spouse_x) {
+							// spouse horizontal line
+							echo '<div style="top:'. ( ($y+12)*7) .'px; left:'. (($x+2) *7) .'px; position: absolute;">  
+								<div style="width: 143px; height: 2px; background-color: grey;"></div>
+							</div> ';	
+						}
+						else {
+							// spouse horizontal line
+							echo '<div style="top:'. ( ($y+12)*7) .'px; left:'. (($spouse_x+2) *7) .'px; position: absolute;">  
+								<div style="width: 143px; height: 2px; background-color: grey;"></div>
+							</div> ';
+						}
+					}
 				}
 			}
 			
+			// get the x,y value out from the loop
 			$display_array[$count+1] = array($x0, $x1, $y);
-			
-			
 			$display_values = array();
 			
 			
 			foreach ($display_array as $key=>$value) {
 				$display_values = $value;
 				
-				
 				$generation_count = ($display_values[1] - $display_values[0])/20;
 				if ($generation_count > 0) {
 							
-							
+					echo "Display: $display_values[0], $display_values[1], $display_values[2] <br> gc: $generation_count<br>";
+					// horizontal line for kids
+					echo '<div style="top:'. (($display_values[2]-3)*7) .'px; left:'. (($display_values[0]+2) *7) .'px; position: absolute;">  
+						<div style="width:'. ( ($display_values[1] - $display_values[0])*7) .'px; height: 1.5px; background-color: grey;"></div>
+					</div> ';
 				
-				echo "Display: $display_values[0], $display_values[1], $display_values[2] <br> gc: $generation_count<br>";
-					// spouse horizontal line
-					echo '<div style="top:'. ($display_values[2]-3) .'%; left:'. ($display_values[0]+2) .'%; position: absolute;">  
-						<div style="width:'. (256*$generation_count) .'px; height: 1px; background-color: grey;"></div>
-					</div> ';		
-							
-					
+					// vertical connection between kids and parents
+					echo '<div style="top:'. (($display_values[2]-8) *7) .'px; left:'. (($display_values[0]+9) *7) .'px; position: absolute;">  
+						<div style="width: 2px; height: 30px; background-color: grey;"></div>
+					</div> ';					
+				}
+				else {
+					// vertical connection between kids and parents
+					echo '<div style="top:'. (($display_values[2]-8) *7) .'px; left:'. (($display_values[0]+2) *7) .'px; position: absolute;">  
+						<div style="width: 2px; height: 30px; background-color: grey;"></div>
+					</div> ';	
 				}
 			}
 		
